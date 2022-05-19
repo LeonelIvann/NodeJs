8bd1fc432376435c46838f67cec49c01570279de
Leonel Ivan Cardoso <cardosoleonel152@gmail.com>
2022-05-19 12:27:49 -0300

    Entrega n° 2

--- index.js
+++ index.js
@@ -1,61 +1,79 @@
-class Usuario {
-    constructor(nombre, apellido, libro, mascotas) {
+const fs = require('fs');
+const { get } = require('http');
+
+class Contenedor {
+    constructor(nombre) {
         this.nombre = nombre;
-        this.apellido = apellido
-        this.libro = libro;
         this.mascotas = mascotas;
+        this.libros = libros;
+        this.mensaje = mensaje;
     }
 }
 
-const datos = [{
-    nombre: 'Roberto',
-    apellido: 'Gomez',
-    libro: [{
-            titulo: 'El libro de la selva',
-            autor: 'Robert',
-            genero: 'Fantasia',
-    }, {    
-            titulo: 'El Rocinante',
-            autor: 'Space X',
-            genero: 'Fantasia',
-            }],
-    mascotas: ['Perro', 'Pez'],
+const Addlibros = [{
+    titulo: 'El libro de la selva',
+    autor: 'Robert',
+    genero: 'Fantasia',
+    id: '1',
+}, {
+    titulo: 'El Jinete',
+    autor: 'Yukimoto',
+    genero: 'Conquista',
+    id: '2',
+}, {
+    titulo: 'Al Espacio y al Infinito',
+    autor: 'Disney',
+    genero: 'Infantil',
+    id: '3',
+}, {
+    titulo: 'El Yugo',
+    autor: 'Mao Tzen',
+    genero: 'Historia',
+    id: '4',
 }];
+const ClearFile = [{
+    mensaje: 'Ya no hay nada más...',
+}]
 
-const getFullName = (usuario) => {
-    const nombre = datos[0].nombre;
-    const apellido = datos[0].apellido;
-    return `${"Nombre : " + nombre} ${apellido}`;
-}
-
-const addMascota = (usuario) => {
-    const mascota = datos[0].mascotas;
-    return `${"Mascotas : " + mascota[0]} , ${mascota[1]}`;
-}
-
-const addBook = (usuario) => {
-    const libro = datos[0].libro;
-    return `${"Titulo : " + " " + libro[0].titulo}`
-}
-
-const countMascotas = (usuario) => {
-    const mascota = datos[0].mascotas;
-    return "Cantidad de mascotas : " + mascota.reduce((total, mascota) => {
-        return total + 1;
+const metodo1 = () => {
+    const pickBook = (libro) => {
+        return libro.id === '3';
     }
-    , 0);
+    const libro = Addlibros.find(pickBook);
+    fs.writeFileSync('contenedor.json', JSON.stringify(libro, null, 2))
+    return "ID del libro " + libro.id;
 }
 
-const getBooksName = (usuario) => {
-    const libro = datos[0].libro;
-    return libro.map(libro => {
-        return libro.titulo;
+const metodo2 = () => {
+    const readFile = fs.readFileSync('contenedor.json');
+    const fileInf = JSON.parse(readFile);
+    if (fileInf.id === '3') {
+        return fileInf;
+    } else {
+        return null;
     }
-    );
 }
 
-console.log(getFullName(datos));
-console.log(addMascota(datos));
-console.log(countMascotas(datos));
-console.log(addBook(datos));
-console.log(getBooksName(datos));
+const metodo3 = () => {
+    const readFile = fs.readFileSync('contenedor.json');
+    const fileInf = JSON.parse(readFile);
+    return fileInf;
+}
+
+const metodo4 = () => {
+    const readFile = fs.readFileSync('contenedor.json');
+    const fileInf = JSON.parse(readFile);
+    const removed = delete fileInf[0];
+    if (removed === true) {
+        fs.writeFileSync('contenedor.json', JSON.stringify(ClearFile, null, 2));
+        return "Se sobrescribió el libro por un mensaje";
+    } else {
+        return "No se pudo eliminar el libro";
+    }
+}
+
+
+console.log("Método 1 : ", metodo1(Addlibros));
+console.log("Método 2 : ", metodo2(Addlibros));
+console.log("Método 3 : ", metodo3(Addlibros));
+console.log("Método 4 : ", metodo4(Addlibros));
