56c86fd406035b0278b2fdddcb09bcd11eff9f61
Leonel Ivan Cardoso <cardosoleonel152@gmail.com>
2022-06-05 03:49:47 -0300

    Incorporando handlebars, Clase 10

--- app.js
+++ app.js
@@ -9,122 +9,47 @@ class Contenedor {
     }
 }
 
-const productos = [
-    {
-        "title": "Escuadra",
-        "price": 123.45,
-        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
-        "id": 1
-    },
-    {
-        "title": "Calculadora",
-        "price": 234.56,
-        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
-        "id": 2
-    },
-    {
-        "title": "Globo Terráqueo",
-        "price": 345.67,
-        "thumbnail": "https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png",
-        "id": 3
-    }
-];
-
-const createArchive = () => {
-    const archivoCreate = fs.createWriteStream('productos.json');
-    const archivoTxtCreate = fs.createWriteStream('productos.txt');
-    archivoCreate.write(JSON.stringify(productos, 2, 3));
-    archivoTxtCreate.write(JSON.stringify(productos, 2, 3));
-    return archivoCreate, archivoTxtCreate;
-}
+const productos = [];
 
 const express = require('express');
-const multer = require('multer');
-const exp = require('constants');
-const { createSecretKey } = require('crypto');
 const app = express();
 const PORT = 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.listen(PORT, () => {
     console.log(`Introduce en tu navegador :  localhost:${PORT}`);
 });
 
-const newProduct = (productos) => {
-    const readFile = fs.readFileSync('productos.json', 'utf8');
-    const fileParse = JSON.parse(readFile);
-    const newProduct = new Contenedor(productos.title, productos.price, productos.thumbnail, productos.id);
-    fileParse.push(newProduct);
-    const newFile = JSON.stringify(fileParse, 2, 3);
-    fs.writeFileSync('productos.json', newFile);
+const newId = () => {
+    let id = 0;
+    for (let i = 0; i < productos.length; i++) {
+        if (productos[i].id > id) {
+            id = productos[i].id;
+        }
+    }
+    return id + 1;
 }
 
-app.use(express.urlencoded({ extended: true }));
-
-app.post('/submit-form', (req, res) => {
-    const username = req.body.nombre
-    const precio = req.body.precio
-    const imagen = req.body.imagen
-    const id = Math.floor(Math.random() * 1000000);
-    const productos = {
-        "title": username,
-        "price": precio,
-        "thumbnail": imagen,
-        "id": id
-    }
-    newProduct(productos);
-    res.redirect('/');
-});
+const createJson = () => {
+    let json = JSON.stringify(productos, null, 2);
+    fs.writeFileSync('./productos.json', json);
+}
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(__dirname + '/public/index.html');
+    res.status(200).render('pages/index.ejs', {productos});
 });
 
-app.get('/api/productos', (req, res) => {
-    const readFile = fs.readFileSync('productos.json', 'utf8');
-    const fileParse = JSON.parse(readFile);
-    res.status(200).send(fileParse);
+app.get('/productos', (req, res) => {
+    res.status(200).render('pages/productos.ejs', {productos});
 });
 
-app.get('/api/productos/:id', (req, res) => {
-    const id = req.params.id;
-    const readFile = fs.readFileSync('productos.json', 'utf8');
-    const fileParse = JSON.parse(readFile);
-    const productos = fileParse.filter(producto => producto.id == id);
-    res.status(200).send(productos);
-});
-
-app.post('/api/productos', (req, res) => {
-    const username = "Vino"
-    const precio = "400"
-    const imagen = "https://cdn.shopify.com/s/files/1/0005/4634/0925/products/DVL-Tinto-2021_2048x2048.jpg?v=1640274188"
-    const id = Math.floor(Math.random() * 1000000);
-    const productos = {
-        "title": username,
-        "price": precio,
-        "thumbnail": imagen,
-        "id": id
-    }
-    newProduct(productos);
-    res.status(200).send(productos);
-});
-
-app.put('/api/productos/:id', (req, res) => {
-    const id = req.params.id;
-    const readFile = fs.readFileSync('productos.json', 'utf8');
-    const fileParse = JSON.parse(readFile);
-    const productos = fileParse.filter(producto => producto.id == id);
-    const newProduct = new Contenedor(req.body.title, req.body.price, req.body.thumbnail, req.body.id);
-    const newFile = JSON.stringify(fileParse, 2, 3);
-    fs.writeFileSync('productos.json', newFile);
-    res.status(200).send(productos);
-});
-
-app.delete('/api/productos/:id', (req, res) => {
-    const id = req.params.id;
-    const readFile = fs.readFileSync('productos.json', 'utf8');
-    const fileParse = JSON.parse(readFile);
-    const productos = fileParse.filter(producto => producto.id != id);
-    const newFile = JSON.stringify(fileParse, 2, 3);
-    fs.writeFileSync('productos.json', newFile);
-    res.status(200).send(productos);
+app.post('/productos', (req, res) => {
+    productos.push({...req.body, id: newId()});
+    res.status(200).render('pages/productos.ejs', {productos});
 });
